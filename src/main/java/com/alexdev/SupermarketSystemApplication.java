80e30b5da8488e883136c4c123aef21ae273f9cf
Alex Barros <alexbarros.dev@gmail.com>
2026-01-06 14:42:10 -0300

    first commit

---
+++ src/main/java/com/alexdev/SupermarketSystemApplication.java
@@ -0,0 +1,13 @@
+package com.alexdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexdev/SupermarketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
