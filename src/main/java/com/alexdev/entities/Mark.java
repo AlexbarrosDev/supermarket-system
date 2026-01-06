c74d81f1b90a5ac9aa7ea7a0f41145b74ce9b189
Alex Barros <alexbarros.dev@gmail.com>
2026-01-06 16:38:12 -0300

    feat: add mark entity

---
+++ src/main/java/com/alexdev/entities/Mark.java
@@ -0,0 +1,45 @@
+package com.alexdev.entities;
+
+import java.util.Objects;
+
+public class Mark {
+
+    private Long id;
+    private String name;
+
+    public Mark() {
+    }
+
+    public Mark(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Mark mark = (Mark) o;
+        return Objects.equals(id, mark.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
