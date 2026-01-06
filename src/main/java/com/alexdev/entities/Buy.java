9161703e349e8734ca902a77f1562d0bc86bfd31
Alex Barros <alexbarros.dev@gmail.com>
2026-01-06 20:28:21 -0300

    feat: add buy entity and buyItem entity

---
+++ src/main/java/com/alexdev/entities/Buy.java
@@ -0,0 +1,65 @@
+package com.alexdev.entities;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Buy {
+
+    private Long id;
+    private Instant date;
+
+    private Client client;
+
+    private List<BuyItem> items = new ArrayList<>();
+
+    public Buy() {
+    }
+
+    public Buy(Long id, Instant date, Client client) {
+        this.id = id;
+        this.date = date;
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<BuyItem> getItems() {
+        return items;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Buy buy = (Buy) o;
+        return Objects.equals(id, buy.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/alexdev/entities/BuyItem.java
@@ -0,0 +1,79 @@
+package com.alexdev.entities;
+
+import java.util.Objects;
+
+public class BuyItem {
+
+    private Long id;
+
+    private Buy buy;
+
+    private Product product;
+
+    private Integer quantity;
+
+    private Double unitPrice;
+
+    public BuyItem() {
+    }
+
+    public BuyItem(Long id, Buy buy, Product product, Integer quantity, Double unitPrice) {
+        this.id = id;
+        this.buy = buy;
+        this.product = product;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Buy getBuy() {
+        return buy;
+    }
+
+    public void setBuy(Buy buy) {
+        this.buy = buy;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(Double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BuyItem buyItem = (BuyItem) o;
+        return Objects.equals(id, buyItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
