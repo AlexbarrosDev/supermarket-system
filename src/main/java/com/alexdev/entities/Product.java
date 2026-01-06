7def88ff036add4d09c5fc1d40e47b42273fc641
Alex Barros <alexbarros.dev@gmail.com>
2026-01-06 17:20:41 -0300

    feat: add product entity

---
+++ src/main/java/com/alexdev/entities/Product.java
@@ -0,0 +1,75 @@
+package com.alexdev.entities;
+
+import java.util.Objects;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private Double price;
+    private Category category;
+    private Mark mark;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, Double price, Category category, Mark mark) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.mark = mark;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Mark getMark() {
+        return mark;
+    }
+
+    public void setMark(Mark mark) {
+        this.mark = mark;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
