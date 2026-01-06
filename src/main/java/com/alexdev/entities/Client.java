dac444c44f06ccf42f5e137330f47545f36b26ed
Alex Barros <alexbarros.dev@gmail.com>
2026-01-06 17:25:45 -0300

    feat: add client entity

---
+++ src/main/java/com/alexdev/entities/Client.java
@@ -0,0 +1,55 @@
+package com.alexdev.entities;
+
+import java.util.Objects;
+
+public class Client {
+
+    private Long id;
+    private String name;
+    private Long cpf;
+
+    public Client() {
+    }
+
+    public Client(Long id, String name, Long cpf) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
