31c5125d0ebc92d3bf385d522183959ce522f69d
Alex Barros <alexbarros.dev@gmail.com>
2026-01-08 07:43:59 -0300

    feat: add client repository

---
+++ src/main/java/com/alexdev/repositories/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.alexdev.repositories;
+
+import com.alexdev.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
