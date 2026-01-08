005ffa8d17f1dd4a4a49174c16ec53f15ddfc335
Alex Barros <alexbarros.dev@gmail.com>
2026-01-08 07:32:07 -0300

    feat: add product repository

---
+++ src/main/java/com/alexdev/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.alexdev.repositories;
+
+import com.alexdev.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
